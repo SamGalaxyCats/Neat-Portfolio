d20d6b43ce353203e7e7a6ccd10b9f70d73bb2a9
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-02-24 11:48:03 -0500

    Update GoFish.java

--- GoFish.java
+++ GoFish.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import java.lang.Math;
 import java.util.ArrayList;
+
+//Running this file will start a game of go fish between the user and the computer.
+
 public class GoFish
 {
    public static void main(String[] args)
